a0812dfde1a6130849b4bea4a26096a3fbc72cb5
DGTJS <joaodiegom15@gmail.com>
2025-08-14 11:32:55 -0300

    feat: connect Mongo Database

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+const connectToDatabase = require("./src/database/mongoose.database");
+
+const TaskModel = require("./src/models/task.models");
+
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.status(200).send("hello word!");
+connectToDatabase();
+
+app.get("/task", async (req, res) => {
+    const taskModel = await TaskModel.find({});
+    res.status(200).send(taskModel);
 });
 
 app.listen(8000, () => {
--- src/database/mongoose.database.js
+++ src/database/mongoose.database.js
@@ -1,1 +1,14 @@
-const connectToDatabase = {};
+const mongoose = require("mongoose");
+
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(
+            `mongodb+srv://Task-manager:${process.env.DATABASE_PASSWORD}@task-manager.omjicff.mongodb.net/?retryWrites=true&w=majority&appName=Task-manager`
+        );
+        console.log("Connected to MongoDB!");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+};
+
+module.exports = connectToDatabase;
