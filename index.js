b98940cf357c5955a3d51aa96874064ee94ba48e
DGTJS <joaodiegom15@gmail.com>
2025-08-14 10:57:07 -0300

    feat: add task model

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("teste!");
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.status(200).send("hello word!");
+});
+
+app.listen(8000, () => {
+    console.log("Listening on port 8000!");
+});
