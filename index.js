fe2e8dfcd2f164c94eaf10a23e2893b3f83e3b10
DGTJS <joaodiegom15@gmail.com>
2025-08-15 10:22:06 -0300

    feat: add route to create a new task

--- index.js
+++ index.js
@@ -8,12 +8,28 @@ const TaskModel = require("./src/models/task.models");
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 connectToDatabase();
 
 app.get("/task", async (req, res) => {
-    const taskModel = await TaskModel.find({});
-    res.status(200).send(taskModel);
+    try {
+        const taskModel = await TaskModel.find({});
+        res.status(200).send(taskModel);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.post("/task", async (req, res) => {
+    try {
+        const CreateTask = new TaskModel(req.body);
+
+        await CreateTask.save();
+        res.status(201).send(CreateTask);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 app.listen(8000, () => {
