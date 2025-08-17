5937385c11aab670d0414c5222a7a260a735c701
DGTJS <joaodiegom15@gmail.com>
2025-08-17 13:28:07 -0300

    feat: move all task endpoint to a controller

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,71 +1,27 @@
 import express from "express";
 import TaskModel from "../models/task.models.js";
+import TaskController from "../controllers/task.controller.js";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    try {
-        const taskModel = await TaskModel.find({});
-        return res.status(200).send(taskModel);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+    return new TaskController(req, res).getTask();
 });
 
 router.get("/:id", async (req, res) => {
-    try {
-        const TaskId = req.params.id;
-
-        const TaskById = await TaskModel.findById(TaskId);
-        if (!TaskById) {
-            return res.status(404).send("Essa tarefa não foi encontrada.");
-        }
-
-        res.status(200).send(TaskById);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+    return new TaskController(req, res).getTaskByid();
 });
 
 router.post("/", async (req, res) => {
-    try {
-        const CreateTask = new TaskModel(req.body);
-
-        await CreateTask.save();
-        res.status(201).send(CreateTask);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+    return new TaskController(req, res).CreateTask();
 });
 
 router.patch("/", async (req, res) => {
-    try {
-        const taskId = req.params.id;
-
-        const taskData = req.body;
-
-        const updateTask = await TaskModel.findByIdAndUpdate(taskId, taskData);
-
-        return res.status(200).send(updateTask);
-    } catch (error) {}
+    return new TaskController(req, res).UpdateTask();
 });
 
 router.delete("/:id", async (req, res) => {
-    try {
-        const Taskid = req.params.id;
-
-        const taskToDelete = await TaskModel.findById(Taskid);
-
-        if (!taskToDelete) {
-            return res.status(404).send("Essa Tarefa não foi encontrada");
-        }
-
-        await TaskModel.findByIdAndDelete(Taskid);
-
-        res.status(200).send("Tarefa deletada com sucesso!");
-    } catch (error) {
-        res.status(500).send("Essa Tarefa não foi encontrada");
-    }
+    return new TaskController(req, res).DeleteTask();
 });
 
 export default router;
